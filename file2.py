9317bdfa463e5bae362ae8e6136dc9818838c925
phanivempati11 <165911010+phanivempati11@users.noreply.github.com>
2026-01-29 19:54:06 +0530

    Create file2.py

---
+++ file2.py
@@ -0,0 +1,14 @@
+# Print Hello World
+print("Hello World")
+
+# Declare variables
+a = 20
+b = 10
+
+# Addition
+sum_result = a + b
+print("Addition:", sum_result)
+
+# Subtraction
+sub_result = a - b
+print("Subtraction:", sub_result)
