c8fbaf2d5b3e2c1da2b50de2c5e676c1a27b23b3
phanivempati11 <165911010+phanivempati11@users.noreply.github.com>
2026-01-29 19:40:18 +0530

    Create file.java

---
+++ file.java
@@ -0,0 +1,20 @@
+public class HelloMath {
+
+    public static void main(String[] args) {
+
+        // Print Hello World
+        System.out.println("Hello World");
+
+        // Declare variables
+        int a = 20;
+        int b = 10;
+
+        // Addition
+        int sum = a + b;
+        System.out.println("Addition: " + sum);
+
+        // Subtraction
+        int sub = a - b;
+        System.out.println("Subtraction: " + sub);
+    }
+}
